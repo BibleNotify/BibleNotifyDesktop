b8861fa17af8f8ef223442d593b8cd3a8bc6be76
I Woithe <iwoithe@just42.net>
2022-03-31 17:50:29 +1030

    Add the source translations

---
+++ translations/biblenotify_en.ts
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Dialogs/AboutDialog.qml" line="73"/>
+        <source>About Bible Notify Desktop</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>AboutView</name>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Views/AboutView.qml" line="26"/>
+        <source>Bible Notify Desktop v0.1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Views/AboutView.qml" line="34"/>
+        <source>Bible Notify is free and open-source software, provided freely for the edification of believers</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Views/AboutView.qml" line="44"/>
+        <source>Licensed under the GPL-3.0 license</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Views/AboutView.qml" line="76"/>
+        <source>Bible Notify Desktop © 2022 Bible Notify Contributors</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>BNButton</name>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/UiComponents/BNButton.qml" line="99"/>
+        <source></source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>HomeView</name>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Views/HomeView.qml" line="32"/>
+        <source>Stop Sending Notifications</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Views/HomeView.qml" line="32"/>
+        <source>Start Sending Notifications</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Views/HomeView.qml" line="40"/>
+        <source>Change Notification Time</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>SetTimeView</name>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Views/SetTimeView.qml" line="24"/>
+        <source>Change when the daily&lt;br&gt;notification is sent:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Views/SetTimeView.qml" line="70"/>
+        <source>Set Notification Time</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biblenotify/ui/BibleNotify/Views/SetTimeView.qml" line="80"/>
+        <source>Cancel</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/biblenotify/ui/main.qml" line="35"/>
+        <source>Open Window</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biblenotify/ui/main.qml" line="40"/>
+        <source>Quit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biblenotify/ui/main.qml" line="121"/>
+        <source>Bible Notify</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biblenotify/ui/main.qml" line="221"/>
+        <source>Bible Notify © 2022 Bible Notify Contributors</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
