7816b89d3b2636e87f33383ead8b0b598d9ea826
I Woithe <iwoithe@just42.net>
2022-03-31 17:56:53 +1030

    Add a todo

---
+++ update_translations.sh
@@ -0,0 +1,4 @@
+# TODO: Document where the translations are stored
+# TODO: Add command line options to update the TS files or compile the translations
+pyside6-lupdate src/biblenotify/biblenotify.qrc -ts translations/biblenotify_en.ts
+pyside6-lrelease translations/biblenotify_en.ts -qm src/translations/biblenotify_en.qm
