3f83843dcefb8a9c6b5af0bb1b98af977dacbdda
Noah Rahm <correctsyntax@yahoo.com>
2022-04-14 12:52:26 -0500

    v0.1.2-alpha

--- update_translations.sh
+++ update_translations.sh
@@ -2,3 +2,4 @@
 # TODO: Add command line options to update the TS files or compile the translations
 pyside6-lupdate src/biblenotify/biblenotify.qrc -ts translations/biblenotify_en.ts
 pyside6-lrelease translations/biblenotify_en.ts -qm src/translations/biblenotify_en.qm
+
