6d7923389d66126cf35f4ec70d8e5a2c75a622f8
I Woithe <iwoithe@just42.net>
2022-04-16 12:03:03 +0930

    Add CLI options to update_translations.sh

--- update_translations.sh
+++ update_translations.sh
@@ -1,5 +1,16 @@
 # TODO: Document where the translations are stored
 # TODO: Add command line options to update the TS files or compile the translations
-pyside6-lupdate src/biblenotify/biblenotify.qrc -ts translations/biblenotify_en.ts
-pyside6-lrelease translations/biblenotify_en.ts -qm src/translations/biblenotify_en.qm
 
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -r|--regen) MODE="regen"; shift;;
+        -c|--compile) MODE="compile"; shift;;
+        *) echo "Unknown parameter passed: $1"; exit 1;;
+    esac
+    shift
+
+if [[ $MODE = "regen" ]]; then
+    pyside6-lupdate src/biblenotify/biblenotify.qrc -ts translations/biblenotify_en.ts
+elif [[ $MODE = "compile" ]]; then
+    pyside6-lrelease translations/biblenotify_en.ts -qm src/translations/biblenotify_en.qm
+fi
